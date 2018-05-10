3ff94d6ef5dc45c5f8672655ccad41ff63dcf392
Greg Rotter <grotter@gmail.com>
2018-05-10 14:45:27 -0700

    configured legend positions

--- js/config.js
+++ js/config.js
@@ -2,5 +2,5 @@ var REDWOOD_CONFIG = {
 	idleSeconds: 30,
 	stepSeconds: 12,
 	points: {"grows":{"left":"888px","top":"215px"},"seasons":{"left":"983px","top":"406px"},"widths":{"left":"392px","top":"483px"},"water":{"left":"820px","top":"324px"},"puzzle":{"left":"994px","top":"435px"},"struggle":{"left":"634px","top":"415px"},"strength":{"left":"771px","top":"94px"},"down":{"left":"638px","top":"102px"},"fog":{"left":"561px","top":"-133px"},"carbon":{"left":"915px","top":"336px"},"clues":{"left":"521px","top":"51px"}},
-	legends: {}
+	legends: {"inner-rings-legend":{"left":"427px","top":"248px"},"summer-to-fall":{"left":"607px","top":"341px"},"spring-to-summer":{"left":"175px","top":"667px"},"false-ring-legend":{"left":"535px","top":"650px"},"1729-legend":{"left":"634px","top":"455px"},"1726-legend":{"left":"821px","top":"342px"},"years-of-plenty-legend":{"left":"239px","top":"652px"},"comparing-climates-legend":{"left":"36px","top":"660px"},"fighting-fungus-legend":{"left":"152px","top":"675px"},"natural-causes-legend":{"left":"92px","top":"405px"},"regrowth-legend":{"left":"181px","top":"683px"},"a-dry-year-legend":{"left":"391px","top":"649px"},"invisible-insights-legend":{"left":"128px","top":"677px"},"local-growth-legend":{"left":"635px","top":"717px"}}
 };
